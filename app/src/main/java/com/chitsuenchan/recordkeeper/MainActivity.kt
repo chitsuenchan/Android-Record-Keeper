2656ba6a82cba0d04971c018c22761c9bb56a80c
chi chan <chitsuenchan@gmail.com>
2023-05-22 12:57:28 +0800

    added clickable navigation bar that switches between fragments

--- app/src/main/java/com/chitsuenchan/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/chitsuenchan/recordkeeper/MainActivity.kt
@@ -2,10 +2,53 @@ package com.chitsuenchan.recordkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.commit
+import com.chitsuenchan.recordkeeper.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.cosntraint_layout_3)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bottomNav.setOnItemSelectedListener(this)
+
     }
+
+
+    private fun onCyclingClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
+    }
+
+    private fun onRunningClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, RunningFragment())
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.nav_cycling) {
+            onCyclingClicked()
+            return true
+        } else if (item.itemId == R.id.nav_running) {
+            onRunningClicked()
+            return true
+        } else {
+            return false
+        }
+    }
+
+
+
 }
