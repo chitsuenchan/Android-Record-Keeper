af6714f8ea0da4f320ae4cafab1ce860dbde55e3
chi chan <chitsuenchan@gmail.com>
2023-05-23 18:25:27 +0800

    added sharepreferences saved data, display records on edit page, return to previous page on save button

--- app/src/main/java/com/chitsuenchan/recordkeeper/running/EditRunningRecordActivity.kt
+++ app/src/main/java/com/chitsuenchan/recordkeeper/running/EditRunningRecordActivity.kt
@@ -1,9 +1,10 @@
 package com.chitsuenchan.recordkeeper.running
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
 import com.chitsuenchan.recordkeeper.databinding.ActivityEditRunningRecordBinding
-import com.chitsuenchan.recordkeeper.databinding.FragmentRunningBinding
 
 class EditRunningRecordActivity : AppCompatActivity() {
 
@@ -20,5 +21,56 @@ class EditRunningRecordActivity : AppCompatActivity() {
         val distance = intent.getStringExtra("Distance")
 
         title = "$distance Record"
+
+        displayRecord(distance)
+        binding.buttonSave.setOnClickListener {
+            saveRecord(distance)
+            finish()        // We add finish here so that after saving the data we exit this fragment
+        }
+
+
+        // Used for application wide saved data
+        // e.g. user settings or user preferences
+//        val applicationPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+//        applicationPreferences.edit {
+//            putString("Some application data", "Application preference value ")
+//        }
+
+        // Used to save data for a specific activity screen
+        // Rarely used in the the industry
+//        val activityPreferences = getPreferences(Context.MODE_PRIVATE)
+//        activityPreferences.edit {
+//            putInt("Some activity data", 15)
+//        }
+
+        // Allow you to create as many data files as needed with custom names
+        // The most useful in the industry
+        // All data files are stored in Device File Explorer under data>data>com.chitsuenchan.recordkeeper>shared_prefs
+//        val fileNamePreferences = getSharedPreferences("Some shared preference file name", Context.MODE_PRIVATE)
+//        fileNamePreferences.edit {
+//            putBoolean("Some preference file name data", false)
+//        }
+
+    }
+
+    private fun displayRecord(distance: String?) {
+        val runningPreferences = getSharedPreferences("running", Context.MODE_PRIVATE)
+
+        // Here are we displaying the record saved in sharedpreferences in the edit boxes
+        binding.editTextRecord.setText(runningPreferences.getString("$distance record", null))
+        binding.editTextDate.setText(runningPreferences.getString("$distance date", null))
+    }
+
+    private fun saveRecord(distance: String?) {
+        val record = binding.editTextRecord.text.toString()
+        val date = binding.editTextDate.text.toString()
+
+        val runningPreferences = getSharedPreferences("running", Context.MODE_PRIVATE)
+
+        // This is how to save data to a new file
+        runningPreferences.edit {
+            putString("$distance record", record)   // This is how to make a unique key. We will now have a key name for 5km, 10km, half-marathon and marathon
+            putString("$distance date", date)
+        }
     }
 }
--- app/src/main/java/com/chitsuenchan/recordkeeper/running/RunningFragment.kt
+++ app/src/main/java/com/chitsuenchan/recordkeeper/running/RunningFragment.kt
@@ -1,5 +1,6 @@
 package com.chitsuenchan.recordkeeper.running
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,6 +12,10 @@ import com.chitsuenchan.recordkeeper.databinding.FragmentRunningBinding
 class RunningFragment : Fragment() {
 
     private lateinit var binding: FragmentRunningBinding
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,11 +35,37 @@ class RunningFragment : Fragment() {
         setupClickListeners()
     }
 
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // We place displayRecords here so everytime we open this fragment it displays the record
+        // Otherwise, if users press the android back button to go back to this fragment. The screen won't refresh/
+        displayRecords()
+    }
+
+
     private fun setupClickListeners() {
         binding.container5km.setOnClickListener { launchRunningRecordScreen("5km") }
         binding.container10km.setOnClickListener { launchRunningRecordScreen("10km") }
         binding.containerHalfMarathon.setOnClickListener { launchRunningRecordScreen("Half Marathon") }
         binding.containerMarathon.setOnClickListener { launchRunningRecordScreen("Marathon") }
+    }
+    private fun displayRecords() {
+        val runningPreferences = requireContext().getSharedPreferences("running", Context.MODE_PRIVATE)     // Here we are accessing the shared preferences file
+
+        binding.textView5kmValue.text = runningPreferences.getString("5km record", "No record set")     // the second argument is a fall-back value if there is none
+        binding.textView5kmDate.text =  runningPreferences.getString("5km date", null)       // fall-back value can also be null
+        binding.textView10kmValue.text = runningPreferences.getString("10km record", null)
+        binding.textView10kmDate.text =  runningPreferences.getString("10km date", null)
+        binding.textViewHalfMarathonValue.text = runningPreferences.getString("Half Marathon record", null)
+        binding.textViewHalfMarathonDate.text =  runningPreferences.getString("Half Marathon date", null)
+        binding.textViewMarathonValue.text = runningPreferences.getString("Marathon record", null)
+        binding.textViewMarathonDate.text =  runningPreferences.getString("Marathon date", null)
+
     }
 
     private fun launchRunningRecordScreen(distance: String) {
